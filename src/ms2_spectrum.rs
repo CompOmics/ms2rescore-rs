cd2a3bec49e12a5fabc8b2f6656d080a936e70df
ArthurDeclercq <arre_declercq@hotmail.com>
2025-12-23 18:06:03 +0100

    add pickle support for ms2spectrum and precursor objects

--- src/ms2_spectrum.rs
+++ src/ms2_spectrum.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 
 use crate::precursor::Precursor;
 
-#[pyclass(get_all, set_all)]
+#[pyclass(module = "ms2rescore_rs", get_all, set_all)]
 #[derive(Debug, Clone)]
 pub struct MS2Spectrum {
     pub identifier: String,
@@ -29,10 +29,26 @@ impl MS2Spectrum {
 
 #[pymethods]
 impl MS2Spectrum {
+    #[new]
+    #[pyo3(signature = (identifier="".to_string(), mz=vec![], intensity=vec![], precursor=None))]
+    pub fn py_new(
+        identifier: String,
+        mz: Vec<f32>,
+        intensity: Vec<f32>,
+        precursor: Option<Precursor>,
+    ) -> Self {
+        MS2Spectrum::new(identifier, mz, intensity, precursor)
+    }
+
     fn __repr__(&self) -> String {
         format!(
             "MS2Spectrum(identifier='{}', mz=[..], intensity=[..], precursor={:?})",
             self.identifier, self.precursor
         )
+    }
+
+    pub fn __reduce__(&self, py: Python<'_>) -> PyResult<(PyObject, (String, Vec<f32>, Vec<f32>, Option<Precursor>))> {
+        let cls = py.import("ms2rescore_rs")?.getattr("MS2Spectrum")?;
+        Ok((cls.into(), (self.identifier.clone(), self.mz.clone(), self.intensity.clone(), self.precursor.clone())))
     }
 }
--- src/precursor.rs
+++ src/precursor.rs
@@ -1,7 +1,7 @@
 use pyo3::prelude::*;
 
 /// Precursor information.
-#[pyclass(get_all, set_all)]
+#[pyclass(module = "ms2rescore_rs", get_all, set_all)]
 #[derive(Debug, Clone)]
 pub struct Precursor {
     pub mz: f64,
@@ -13,11 +13,28 @@ pub struct Precursor {
 
 #[pymethods]
 impl Precursor {
+    #[new]
+    #[pyo3(signature = (mz=0.0, rt=0.0, im=0.0, charge=0, intensity=0.0))]
+    pub fn new(mz: f64, rt: f64, im: f64, charge: usize, intensity: f64) -> Self {
+        Precursor {
+            mz,
+            rt,
+            im,
+            charge,
+            intensity,
+        }
+    }
+
     pub fn __repr__(&self) -> String {
         format!(
             "Precursor(mz={}, rt={}, im={}, charge={}, intensity={})",
             self.mz, self.rt, self.im, self.charge, self.intensity
         )
+    }
+
+    pub fn __reduce__(&self, py: Python<'_>) -> PyResult<(PyObject, (f64, f64, f64, usize, f64))> {
+        let cls = py.import("ms2rescore_rs")?.getattr("Precursor")?;
+        Ok((cls.into(), (self.mz, self.rt, self.im, self.charge, self.intensity)))
     }
 }
 
