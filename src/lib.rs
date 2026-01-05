f3815e1031e25825ee430b007f0de2f5140e5716
ArthurDeclercq <arre_declercq@hotmail.com>
2026-01-05 15:06:23 +0100

    rayon multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod precursor;
 
 use std::collections::HashMap;
 
-use pyo3::exceptions::{PyException, PyValueError};
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 
 use file_types::{match_file_type, SpectrumFileType};
@@ -23,17 +23,20 @@ pub fn is_supported_file_type(spectrum_path: String) -> bool {
 
 /// Get mapping of spectrum identifiers to precursor information.
 #[pyfunction]
-pub fn get_precursor_info(spectrum_path: String) -> PyResult<HashMap<String, Precursor>> {
+pub fn get_precursor_info(py: Python<'_>, spectrum_path: String) -> PyResult<HashMap<String, Precursor>> {
     let file_type = match_file_type(&spectrum_path);
 
-    let precursors = match file_type {
+    let precursors = py.allow_threads(|| match file_type {
         SpectrumFileType::MascotGenericFormat
         | SpectrumFileType::MzML
         | SpectrumFileType::MzMLb
         | SpectrumFileType::ThermoRaw => parse_mzdata::parse_precursor_info(&spectrum_path),
         SpectrumFileType::BrukerRaw => parse_timsrust::parse_precursor_info(&spectrum_path),
-        SpectrumFileType::Unknown => return Err(PyValueError::new_err("Unsupported file type")),
-    };
+        SpectrumFileType::Unknown => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Unsupported file type",
+        )),
+    });
 
     match precursors {
         Ok(precursors) => Ok(precursors),
@@ -43,17 +46,20 @@ pub fn get_precursor_info(spectrum_path: String) -> PyResult<HashMap<String, Pre
 
 /// Get MS2 spectra from a spectrum file.
 #[pyfunction]
-pub fn get_ms2_spectra(spectrum_path: String) -> PyResult<Vec<ms2_spectrum::MS2Spectrum>> {
+pub fn get_ms2_spectra(py: Python<'_>, spectrum_path: String) -> PyResult<Vec<ms2_spectrum::MS2Spectrum>> {
     let file_type = match_file_type(&spectrum_path);
 
-    let spectra = match file_type {
+    let spectra = py.allow_threads(|| match file_type {
         SpectrumFileType::MascotGenericFormat
         | SpectrumFileType::MzML
         | SpectrumFileType::MzMLb
         | SpectrumFileType::ThermoRaw => parse_mzdata::read_ms2_spectra(&spectrum_path),
         SpectrumFileType::BrukerRaw => parse_timsrust::read_ms2_spectra(&spectrum_path),
-        SpectrumFileType::Unknown => return Err(PyValueError::new_err("Unsupported file type")),
-    };
+        SpectrumFileType::Unknown => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Unsupported file type",
+        )),
+    });
 
     match spectra {
         Ok(spectra) => Ok(spectra),
